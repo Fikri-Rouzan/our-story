11be1ccb9e5e484f862adeb214b419be61b383e0
Muhammad Fikri Rouzan Ash Shidik <fikrirzn@gmail.com>
2025-06-04 20:02:45 +0700

    twenty-fifth commit

--- public/sw.js
+++ public/sw.js
@@ -4,73 +4,82 @@ importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js"
 );
 
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
+if (workbox) {
+  console.log(`Workbox is loaded`);
 
-workbox.routing.registerRoute(
-  ({ request }) => request.destination === "image",
-  new workbox.strategies.CacheFirst({
-    cacheName: "images-cache",
-    plugins: [
-      new workbox.expiration.ExpirationPlugin({
-        maxEntries: 100,
-        maxAgeSeconds: 7 * 24 * 60 * 60,
-      }),
-    ],
-  })
-);
+  workbox.core.skipWaiting();
+  workbox.core.clientsClaim();
 
-workbox.routing.registerRoute(
-  ({ url }) =>
-    url.origin === "https://story-api.dicoding.dev" &&
-    url.pathname.startsWith("/v1/stories"),
-  new workbox.strategies.NetworkFirst({
-    cacheName: "api-stories-cache",
-    networkTimeoutSeconds: 10,
-    plugins: [
-      new workbox.expiration.ExpirationPlugin({
-        maxEntries: 50,
-        maxAgeSeconds: 24 * 60 * 60,
-      }),
-    ],
-  })
-);
+  workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
 
-workbox.routing.registerRoute(
-  ({ request }) =>
-    request.destination === "script" ||
-    request.destination === "style" ||
-    request.destination === "document",
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: "assets-cache",
-  })
-);
+  workbox.routing.registerRoute(
+    ({ request }) => request.destination === "image",
+    new workbox.strategies.CacheFirst({
+      cacheName: "images-cache",
+      plugins: [
+        new workbox.expiration.ExpirationPlugin({
+          maxEntries: 100,
+          maxAgeSeconds: 7 * 24 * 60 * 60,
+        }),
+      ],
+    })
+  );
 
-workbox.routing.registerRoute(
-  ({ url }) =>
-    url.origin.includes("tile.openstreetmap.org") ||
-    url.origin.includes("api.maptiler.com"),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: "map-tiles-cache",
-    plugins: [
-      new workbox.expiration.ExpirationPlugin({
-        maxEntries: 200,
-        maxAgeSeconds: 7 * 24 * 60 * 60,
-      }),
-    ],
-  })
-);
+  workbox.routing.registerRoute(
+    ({ url }) =>
+      url.origin === "https://story-api.dicoding.dev" &&
+      url.pathname.startsWith("/v1/stories"),
+    new workbox.strategies.NetworkFirst({
+      cacheName: "api-stories-cache",
+      networkTimeoutSeconds: 10,
+      plugins: [
+        new workbox.expiration.ExpirationPlugin({
+          maxEntries: 50,
+          maxAgeSeconds: 24 * 60 * 60,
+        }),
+      ],
+    })
+  );
 
-workbox.routing.registerRoute(
-  ({ request }) => request.mode === "navigate",
-  new workbox.strategies.NetworkFirst({
-    cacheName: "html-cache",
-    plugins: [
-      new workbox.expiration.ExpirationPlugin({
-        maxEntries: 10,
-      }),
-    ],
-  })
-);
+  workbox.routing.registerRoute(
+    ({ request }) =>
+      request.destination === "script" ||
+      request.destination === "style" ||
+      request.destination === "document",
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: "assets-cache",
+    })
+  );
+
+  workbox.routing.registerRoute(
+    ({ url }) =>
+      url.origin.includes("tile.openstreetmap.org") ||
+      url.origin.includes("api.maptiler.com"),
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: "map-tiles-cache",
+      plugins: [
+        new workbox.expiration.ExpirationPlugin({
+          maxEntries: 200,
+          maxAgeSeconds: 7 * 24 * 60 * 60,
+        }),
+      ],
+    })
+  );
+
+  workbox.routing.registerRoute(
+    ({ request }) => request.mode === "navigate",
+    new workbox.strategies.NetworkFirst({
+      cacheName: "html-cache",
+      plugins: [
+        new workbox.expiration.ExpirationPlugin({
+          maxEntries: 10,
+        }),
+      ],
+    })
+  );
+} else {
+  console.log(`Workbox didn't load`);
+}
 
 self.addEventListener("push", (event) => {
   const defaultPayload = {
--- workbox-config.cjs
+++ workbox-config.cjs
@@ -3,8 +3,4 @@ module.exports = {
   swDest: "dist/sw.js",
   globDirectory: "dist/",
   globPatterns: ["**/*.{html,js,css,png,svg,ico,json}"],
-  clientsClaim: true,
-  skipWaiting: true,
-
-  runtimeCaching: [],
 };
